a2677bb3296f308833cbe8652af5addac2c72484
Esra <esra@github.com>
2017-11-03 13:04:35 +0300

    first commit

---
+++ Downloads/BottomNavigation-Demo-master/app/src/main/java/priyank/patel/bottomnavigationdemo/HomeFragment.java
@@ -0,0 +1,24 @@
+package priyank.patel.bottomnavigationdemo;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by esraayik on 2.11.2017.
+ */
+
+
+@SuppressLint("ValidFragment")
+class HomeFragment extends Fragment {
+    public HomeFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
